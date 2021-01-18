4b6388af374d4a22fdab4d0af9bfd8ffc472ab8d
Derian Melvin <derianmelvin88@gmail.com>
2021-01-18 10:47:32 +0700

    - Change component to class component - Import Toolbar - Import Editor - Import Preview - Add imported components to render()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, { Component } from 'react';
+
+import Toolbar from './components/Toolbar.jsx'
+import Editor from './components/Editor.jsx'
+import Preview from './components/Preview.jsx'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+class App extends Component {
+  state = {
 
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        <div id="container-editor">
+          <Toolbar />
+          <Editor />
+        </div>
+        <div className="container-preview">
+          <Toolbar />
+          <Preview />
+        </div>
+      </div>
+    );
+  }
+}
+ 
 export default App;
