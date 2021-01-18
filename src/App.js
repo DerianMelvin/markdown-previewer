146c9e44808f15e66bd55ca98eb32fa0da9f3f17
Derian Melvin <derianmelvin88@gmail.com>
2021-01-18 17:51:56 +0700

    - Add const placeholder - Add to state { markdown: placeholder } - Add handleChange method - Add props to <Toolbar /> - Add props to <Editor /> - Add props to <Preview />

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Toolbar from './components/Toolbar.jsx'
-import Editor from './components/Editor.jsx'
-import Preview from './components/Preview.jsx'
+import Toolbar from "./components/Toolbar.jsx";
+import Editor from "./components/Editor.jsx";
+import Preview from "./components/Preview.jsx";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
+    markdown: placeholder,
+  };
 
-  }
+  handleChange = (e) => {
+    this.setState({
+      markdown: e.target.value,
+    });
+  };
 
-  render() { 
+  render() {
     return (
       <div className="App">
+        <h1>Markdown Previewer</h1>
+
         <div id="container-editor">
-          <Toolbar />
-          <Editor />
+          <Toolbar header="Editor" />
+          <Editor markdown={this.state.markdown} handleChange={this.handleChange} />
         </div>
-        <div className="container-preview">
-          <Toolbar />
-          <Preview />
+
+        <div id="container-preview">
+          <Toolbar header="Preview" />
+          <Preview markdown={this.state.markdown} />
         </div>
       </div>
     );
   }
 }
- 
+
+let placeholder = "This is a sample sentence for placeholder";
+
 export default App;
